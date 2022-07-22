d42b9d0a37439e442b14bc0d542c04af22bfa788
GaryChuan <garychuanzh@gmail.com>
2022-07-23 00:09:27 +0800

    Moved pch.h to src

--- src/pch.h
+++ src/pch.h
@@ -1,12 +1,9 @@
 #pragma once
+
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 #include "xecs.h"
 #include <random>
-#include <tuple>
 
 #pragma warning (disable:4244)  // Disable bogus VC++ 4.2 conversion warnings. 
 #include <GL/gl.h>
 #pragma comment (lib, "opengl32.lib") // link with Microsoft OpenGL lib
-
-#define GLUT_STATIC_LIB
-#include "GL/glut.h"

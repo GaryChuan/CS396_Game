bacecbb243fb6bed852e9dd2392779aed165951a
Gary Chuan <garychuanzh@gmail.com>
2022-08-05 23:21:46 +0800

    zombie logic update before go home :(

--- src/main.cpp
+++ src/main.cpp
@@ -237,7 +237,7 @@ private:
 			ZombieGroupLogic,
 			ZombieGroupSpawnTimerLogic,
 			Renderer,
-				RenderGrid,
+				// RenderGrid,
 				RenderPlayer,
 				RenderZombies,
 				RenderBullets,
--- src/systems/zombie_logic.h
+++ src/systems/zombie_logic.h
@@ -22,18 +22,45 @@ public:
 
 	void OnGameStart() noexcept
 	{
+		mQueryZombies.m_Must.AddFromComponents<Zombie>();
+		mQueryZombies.m_NoneOf.AddFromComponents<PlayerTag, Bullet>();
+
 		mQueryPlayer.m_Must.AddFromComponents<PlayerTag>();
 	}
 
-	__inline void operator()(const Position& zombiePos, Velocity& vel)
+	void OnUpdate() noexcept
 	{
-		Foreach(Search(mQueryPlayer), 
-			[&](const Position& playerPos)
+		xcore::vector2 playerPos{};
+
+		Foreach(Search(mQueryPlayer),
+			[&](const Position& position)
 			{
-				vel.mValue = (playerPos.mValue - zombiePos.mValue).NormalizeSafe() * 0.5f;
+				playerPos = position.mValue;
+				// vel.mValue = (playerPos.mValue - zombiePos.mValue).NormalizeSafe() * 0.5f;
+			});
+
+		Foreach(Search(mQueryZombies),
+			[&](xecs::component::entity& zombie1, Velocity& zombieVel, const Position& zombiePos) noexcept
+			{
+				Foreach(
+					Search(mQueryZombies),
+					[&](xecs::component::entity& zombie2, const Position& zombiePos2)
+					{
+						if (zombie2.isZombie()
+						 || zombie1 == zombie2)
+						{
+							return;
+						}
+
+
+
+					});
+
+				zombieVel.mValue = (playerPos - zombiePos.mValue).NormalizeSafe() * 0.5f;
 			});
 	}
 
 private:
+	xecs::query::instance mQueryZombies{};
 	xecs::query::instance mQueryPlayer{};
 };
